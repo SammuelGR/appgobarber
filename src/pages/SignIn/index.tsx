830932c012f4a627916348a2e1735bc0d9dc120f
Sammuel Reis <sammugr@hotmail.com>
2020-07-13 19:16:20 -0300

    finishes sigin page layout

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,31 +1,69 @@
 import React from 'react';
-import { Image } from 'react-native';
+import {
+	Image,
+	KeyboardAvoidingView,
+	Platform,
+	ScrollView,
+	View,
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
 
-import { Container, Title } from './styles';
+import {
+	Container,
+	Title,
+	ForgotPassword,
+	ForgotPasswordText,
+	CreateAccountButton,
+	CreateAccountButtonText,
+} from './styles';
 
 const SignIn: React.FC = () => {
 	return (
-		<Container>
-			<Image source={logoImg} />
-
-			<Title>Faça seu logon</Title>
-
-			<Input name="email" icon="mail" placeholder="E-mail" />
-			<Input name="password" icon="lock" placeholder="Senha" />
-
-			<Button
-				onPress={() => {
-					console.log('test');
-				}}
+		<>
+			<KeyboardAvoidingView
+				behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+				style={{ flex: 1 }}
+				enabled
 			>
-				Entrar
-			</Button>
-		</Container>
+				<ScrollView
+					contentContainerStyle={{ flex: 1 }}
+					keyboardShouldPersistTaps="handled"
+				>
+					<Container>
+						<Image source={logoImg} />
+
+						<View>
+							<Title>Faça seu logon</Title>
+						</View>
+
+						<Input name="email" icon="mail" placeholder="E-mail" />
+						<Input name="password" icon="lock" placeholder="Senha" />
+
+						<Button
+							onPress={() => {
+								console.log('test');
+							}}
+						>
+							Entrar
+						</Button>
+
+						<ForgotPassword onPress={() => {}}>
+							<ForgotPasswordText>Esqueci minha senha</ForgotPasswordText>
+						</ForgotPassword>
+					</Container>
+				</ScrollView>
+			</KeyboardAvoidingView>
+
+			<CreateAccountButton onPress={() => {}}>
+				<Icon name="log-in" size={20} color="#ff9000" />
+				<CreateAccountButtonText>Criar uma conta</CreateAccountButtonText>
+			</CreateAccountButton>
+		</>
 	);
 };
 
